b133b5982b4e4cffb4b90a3609fd0c05c9ca96a9
Leandro Gonçalves <159172886+lleandrogon@users.noreply.github.com>
2025-01-09 22:28:38 -0300

    Classes, Atributos e Métodos

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+
+class Pessoa {
+    public $nome;
+    public $idade;
+
+    public function Falar() {
+        echo $this->nome . " de " . $this->idade . " acabou de falar <br>";
+    }
+}
+
+$rodrigo = new Pessoa();
+$rodrigo->nome = "Rodrigo Santos de Oliveira";
+$rodrigo->idade = 25;
+$rodrigo->Falar();
