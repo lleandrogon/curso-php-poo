dda949f989c2cd2784bd5e0696b2fd054fd32147
Leandro Gonçalves <lleandrogon2004@gmail.com>
2025-01-16 11:46:21 -0300

    Adicionando Composer

--- index.php
+++ index.php
@@ -1,35 +1,9 @@
 <?php
 
-class Produtos {
-    public $nome;
-    public $valor;
+require_once "vendor/autoload.php";
 
-    function __construct($nome, $valor) {
-        $this->nome = $nome;
-        $this->valor = $valor;
-    }
-}
+use Cocur\Slugify\Slugify;
 
-class Carrinho {
-    public $produtos;
-
-    public function adiciona(Produtos $produto) {
-        $this->produtos[] = $produto;
-    }
-
-    public function exibe() {
-        foreach ($this->produtos as $produto) {
-            echo $produto->nome . "<br>";
-            echo $produto->valor . "<hr>";
-        }
-    }
-}
-
-$produto1 = new Produtos("Notebook", "1500");
-$produto2 = new Produtos("Mouse", "50");
-
-$carrinho = new Carrinho();
-$carrinho->adiciona($produto1);
-$carrinho->adiciona($produto2);
-
-$carrinho->exibe();
+$slug = new Slugify();
+$slug->addRule("ão", "funcionou");
+echo $slug->slugify("Teste com acentuação para geração de slug", "_");
